4b433a844b32af5c48302cf24cac4e9c9cb6fd40
Pablo Sánchez Carmona <pablosc@amazon.es>
2022-08-26 17:27:33 +0200

    minor edits

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,7 +27,7 @@ variable "accept_attachments" {
   type        = bool
   description = "Indicates if the VPC attachments are accepted - for those segments that require acceptance (as per Cloud WAN policy)."
 
-  default = false
+  default = true
 }
 
 # Definition of the VPCs to create in N. Virginia Region
